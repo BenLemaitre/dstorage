5290880a19f4b1ef3bde972f4b3f4b49a9f5b502
Ben <ben@pop-os.localdomain>
2021-09-22 22:58:11 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+
+import logo from "../logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Dstorage App.</h1>
+    </div>
+  );
+}
+
+export default App;
